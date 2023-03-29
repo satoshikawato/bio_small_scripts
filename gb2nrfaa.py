94a8916a6984a7b0355db21109211ce25308adfc
satoshikawato <58936715+satoshikawato@users.noreply.github.com>
2023-03-29 15:08:10 +0900

    Update gb2nrfaa.py

--- gb2nrfaa.py
+++ gb2nrfaa.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from contextlib import redirect_stdout
 
 
 def _get_args():
@@ -16,13 +17,14 @@ def _get_args():
     parser.add_argument(
         '-i', '--input', help='GenBank flat file format of the genomic sequence(s) (required)', type=str, required=True)
     parser.add_argument(
-        '-o', '--output', help='output fasta-formatted file (required)', type=str, default="out.faa")
+        '-o', '--output', help='output fasta-formatted file (optional)', type=str, default="out.faa")
+    parser.add_argument(
+        '-t', '--table', help='output tab-separated table (optional)', type=str, default="out.tbl")
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     args = parser.parse_args()
     return args
-
 
 def _gb_record_to_proteins(gb_record):
     protein_records = defaultdict(list)
@@ -44,7 +46,6 @@ def _gb_record_to_proteins(gb_record):
         protein_records[gene_id])) for gene_id in protein_records.keys()]
     return out_records
 
-
 def _gb_to_proteins(in_gb):
     gb_records = SeqIO.parse(in_gb, 'genbank')
     protein_records = [_gb_record_to_proteins(
@@ -53,15 +54,19 @@ def _gb_to_proteins(in_gb):
         protein for proteins in protein_records for protein in proteins]
     return protein_records
 
-
 def main():
     args = _get_args()
     in_gb = args.input
     out_file = args.output
+    out_table = args.table
     protein_records = _gb_to_proteins(in_gb)
+    with open(out_table, "w") as output_table:
+        with redirect_stdout(output_table):
+            for record in protein_records:
+                print('{}\t{}'.format(record.name, record.id))
     with open(out_file, "w") as output_handle:
         SeqIO.write(protein_records, output_handle, "fasta")
 
-
 if __name__ == "__main__":
     main()
+
