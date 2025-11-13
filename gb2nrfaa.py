b55c4fd9cff05378ca409225c9fc0d51ebf52656
Satoshi Kawato <58936715+satoshikawato@users.noreply.github.com>
2025-11-13 18:38:16 +0900

    Refactor protein extraction and output handling

--- gb2nrfaa.py
+++ gb2nrfaa.py
@@ -2,14 +2,18 @@
 # coding: utf-8
 
 import sys
+import os
 import argparse
+from collections import defaultdict
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from contextlib import redirect_stdout
+
 
 def _get_args():
     parser = argparse.ArgumentParser(
-        description='Extract the longest isoforms of protein-coding genes from a NCBI RefSeq eukaryotic genome assembly')
+        description='Extract the longest isoforms of protein-coding genes from a NCBI RefSeq euaryotic genome assembly')
     parser.add_argument(
         '-i', '--input', help='GenBank flat file format of the genomic sequence(s) (required)', type=str, required=True)
     parser.add_argument(
@@ -22,58 +26,47 @@ def _get_args():
     args = parser.parse_args()
     return args
 
-def _process_record_and_write(gb_record, out_handle, tbl_handle):
-    """
-    Extract the longest isoforms from a single GenBank record and write them to files immediately.
-    """
-    # {gene_id: (length, seq_record)}
-    best_proteins = {}
-
-    organism = gb_record.annotations.get('organism', 'unknown_organism')
-
+def _gb_record_to_proteins(gb_record):
+    protein_records = defaultdict(list)
     for feature in gb_record.features:
-        if feature.type != 'CDS':
+        if feature.type == 'CDS':
+            if 'protein_id' in feature.qualifiers.keys():
+                protein_id = feature.qualifiers['protein_id'][0]
+            else:
+                continue
+            gene_id = feature.qualifiers['gene'][0]
+            product_name = feature.qualifiers['product'][0]
+            aa_seq = Seq(feature.qualifiers['translation'][0])
+            aa_record = SeqRecord(aa_seq, id=protein_id,
+                                  name=gene_id, description="{} [{}]".format(product_name, gb_record.annotations['organism']))
+            protein_records[gene_id].append(aa_record)
+        else:
             continue
+    out_records = [max(protein_records[gene_id], key=lambda k: len(
+        protein_records[gene_id])) for gene_id in protein_records.keys()]
+    return out_records
 
-        qualifiers = feature.qualifiers
-        
-        if 'protein_id' not in qualifiers or 'gene' not in qualifiers or 'translation' not in qualifiers:
-            continue
-
-        protein_id = qualifiers['protein_id'][0]
-        gene_id = qualifiers['gene'][0]
-        product_name = qualifiers.get('product', ['unknown_product'])[0]
-        translation_seq = qualifiers['translation'][0]
-        
-        seq_len = len(translation_seq)
-
-        if gene_id not in best_proteins or seq_len > best_proteins[gene_id][0]:
-            aa_seq = Seq(translation_seq)
-            aa_record = SeqRecord(
-                aa_seq, 
-                id=protein_id,
-                name=gene_id, 
-                description="{} [{}]".format(product_name, organism)
-            )
-            best_proteins[gene_id] = (seq_len, aa_record)
-
-    if best_proteins:
-        records_to_write = [item[1] for item in best_proteins.values()]
-
-        SeqIO.write(records_to_write, out_handle, "fasta")
-        
-        for record in records_to_write:
-            tbl_handle.write('{}\t{}\n'.format(record.name, record.id))
+def _gb_to_proteins(in_gb):
+    gb_records = SeqIO.parse(in_gb, 'genbank')
+    protein_records = [_gb_record_to_proteins(
+        gb_record) for gb_record in gb_records]
+    protein_records = [
+        protein for proteins in protein_records for protein in proteins]
+    return protein_records
 
 def main():
     args = _get_args()
     in_gb = args.input
     out_file = args.output
     out_table = args.table
-
-    with open(out_file, "w") as out_handle, open(out_table, "w") as tbl_handle:
-        for gb_record in SeqIO.parse(in_gb, 'genbank'):
-            _process_record_and_write(gb_record, out_handle, tbl_handle)
+    protein_records = _gb_to_proteins(in_gb)
+    with open(out_table, "w") as output_table:
+        with redirect_stdout(output_table):
+            for record in protein_records:
+                print('{}\t{}'.format(record.name, record.id))
+    with open(out_file, "w") as output_handle:
+        SeqIO.write(protein_records, output_handle, "fasta")
 
 if __name__ == "__main__":
     main()
+
